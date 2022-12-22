9abd83788a8927191c39e511cfacf09997c6691e
Bran <june20516@gmail.com>
2022-12-22 16:45:38 +0900

    apply api wrapper

---
+++ pages/api/users/enter.tsx
@@ -0,0 +1,9 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import withHandler from '../../../libs/server/withHandler';
+
+export default withHandler('POST', (req: NextApiRequest, res: NextApiResponse) => {
+  console.log(req.body);
+  res.json({
+    status: 'ok',
+  });
+});
